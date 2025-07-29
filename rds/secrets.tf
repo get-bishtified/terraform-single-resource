11e20cc2ddce5f15734447c1aa34854294d3cba1
get-bishtified <support@bishtify.com>
2025-07-29 16:40:05 +0530

    Delete rds directory

--- rds/secrets.tf
+++
@@ -1,7 +0,0 @@
-data "aws_secretsmanager_secret" "db_password" {
-  name = var.db_secret_name
-}
-
-data "aws_secretsmanager_secret_version" "db_password_version" {
-  secret_id = data.aws_secretsmanager_secret.db_password.id
-}
