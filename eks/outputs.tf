fe9adfab5a1945291e3e87d56bd4e6598a114dc8
get-bishtified <support@bishtify.com>
2025-08-06 11:06:45 +0530

    Delete eks directory

--- eks/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "kubeconfig_command" {
-  description = "Command to update kubeconfig"
-  value       = "aws eks --region ${var.aws_region} update-kubeconfig --name ${module.eks.cluster_name}"
-}
-
-output "cluster_name" {
-  description = "EKS cluster name"
-  value       = module.eks.cluster_name
-}
