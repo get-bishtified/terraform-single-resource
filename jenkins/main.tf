2fb069c8911a7c338807f3ff221822d96d4a3d28
Shankaz <shankazcloud@gmail.com>
2025-12-26 19:53:15 +0530

    added

--- jenkins/main.tf
+++ jenkins/main.tf
@@ -8,6 +8,13 @@ resource "aws_security_group" "jenkins_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+    ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   ingress {
     from_port   = 443
     to_port     = 443
